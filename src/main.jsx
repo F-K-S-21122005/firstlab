ea79da849597f198236a2dee0e7ac05017c96d88
KOSTYA F <chappychips@yandex.ru>
2023-10-15 22:38:56 +0600

    Minor Roating add

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import TopBar from './Top-bar/Top-bar.jsx';
 import Card from './Card/Card.jsx';
 import './index.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
